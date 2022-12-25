cb78ed97d773eb7e8e79465f642418b1a42e18aa
VidsterBroyo <vidsterbroyo@gmail.com>
2022-12-24 19:31:07 -0500

    Cleaned up code

--- myAPI.js
+++ myAPI.js
@@ -1,42 +1,61 @@
 // npm libraries
 const fsP = require('fs').promises
-
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const upload = multer()
+
+// express library setup
 const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.use("/static", express.static('./static/'))
 
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'ca-central-1'});
-var ddb = new AWS.DynamoDB.DocumentClient;
+// set ddb libraries
+var AWS = require('aws-sdk')
+AWS.config.update({region: 'ca-central-1'})
+var ddb = new AWS.DynamoDB.DocumentClient
 
 const PORT = process.env.PORT || 5000
 
 
 // home page
 app.get('/', async (req, res) => { 
+    // read from index.html file
     let data = await fsP.readFile('./index.html')
+
+    // send html
     res.writeHead(200, {'Content-Type': 'text/html'}).end(data)
 })
 
 
 // get request
 app.get('/getBooks', async (req, res) => { 
-
+    // define ddb parameters
     var params = {TableName: 'bookShelfDB'}
 
-    let data = await ddb.scan(params).promise()
-    res.json(data["Items"])
+    // set CORS permissions
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    try{
+        // read from ddb table
+        let data = await ddb.scan(params).promise()
+
+        // send json
+        res.setHeader('content-type', 'application/json')
+        res.json(data)
+        res.end()
+    } catch (error) {
+        res.end("getBooks response error: "+error)
+    }
 })
 
 
-// post request
+// new book (post) request
 app.post('/postReq', upload.none(), async(req, res) => {
 
+    // define ddb parameters
     var params = {
         TableName: 'bookShelfDB',
         Item: {
@@ -47,14 +66,18 @@ app.post('/postReq', upload.none(), async(req, res) => {
         }
       }
     
+    // add book to ddb table
     data = await ddb.put(params).promise()
+
+    // refresh page
     res.redirect('/')
 })
 
 
-// put request (updating rating)
+// updating rating (put) request
 app.put('/putReq/:author/:title/:rating', async (req, res) => {
-
+    
+    // define ddb parameters
     const params = {
         TableName: "bookShelfDB",
         Key: {
@@ -65,13 +88,18 @@ app.put('/putReq/:author/:title/:rating', async (req, res) => {
         ExpressionAttributeValues: { ":x": req.params.rating }
     }
 
+    // update rating on ddb table
     await ddb.update(params).promise()
+
+    // refresh page
     res.redirect('/')
 })
 
 
 // delete request
 app.delete('/delReq/:title/:author', async (req, res) => {
+
+    // define ddb parameters
     const params = {
         TableName: 'bookShelfDB',
         Key: {
@@ -80,8 +108,11 @@ app.delete('/delReq/:title/:author', async (req, res) => {
         }
     }
 
+    // delete book on ddb table
     await ddb.delete(params).promise()
-    res.end()
+
+    // refresh page
+    res.redirect('/')
 })
 
 
--- static/index.js
+++ static/index.js
@@ -1,61 +1,81 @@
+// variables to save which book's rating should be changed
 let changeRatingTitle
 let changeRatingAuthor
 
+
+// loading all books into the html
 function loadBooks(){
+    try{
+        // send getBooks request
+        var request = new XMLHttpRequest()
+        request.open('GET', 'http://ec2-35-182-99-203.ca-central-1.compute.amazonaws.com:5000/getBooks', false)
+        request.send()
 
-    var request = new XMLHttpRequest()
+        // save list of book objects
+        books = JSON.parse(request.responseText)["Items"]
+        
+        // loop through books
+        for(x in books){
+            // if loop has reached 4 books, make a new row
+            if(x % 4 == 0){
+                document.getElementById("bookGrid").innerHTML += `<div class="row mb-5" id="bookRow${x/4}"></div>`
+            }
 
-    request.open('GET', 'http://localhost:5000/getBooks', false)
-    request.send()
-
-    books = JSON.parse(request.responseText)
-    console.log(books)
-
-    for(x in books){
-        if(x % 4 == 0){
-            document.getElementById("bookGrid").innerHTML += `<div class="row mb-5" id="bookRow${x/4}"></div>`
-        }
-
-        newCard = `
-        <div class="col-3" class="card">
-            <img src="${books[x]['cover']}" class="card-img-top" style="object-fit: cover; width: 100%; height:75%;" alt="book cover">
-            <div class="card-body">
-                <h5 class="card-title">${books[x]['title']}</h5>
-                <p class="author">${books[x]['author']}</p>
-                <p class="card-text">${"⭐".repeat(books[x]['rating'])}</p>
-                <button id="${books[x]['title'] + "\\" + books[x]['author']}" onclick="setBookChange(this.id)" class="btn change-btn" data-bs-toggle="modal" data-bs-target="#changeRating">Change Rating</button>
-                <button id="${books[x]['title'] + "\\" + books[x]['author']}" onclick="deleteBook(this.id)" class="btn delete-btn">Delete</button>
+            // html code for the new book card
+            newCard = `
+            <div class="col-3" class="card">
+                <img src="${books[x].cover}" class="card-img-top" style="object-fit: cover; width: 100%; height:75%;" alt="book cover">
+                <div class="card-body">
+                    <h5 class="card-title">${books[x].title}</h5>
+                    <p class="author">${books[x].author}</p>
+                    <p class="card-text">${"⭐".repeat(books[x].rating)}</p>
+                    <button id="${books[x].title + "\\" + books[x].author}" onclick="setBookChange(this.id)" class="btn change-btn" data-bs-toggle="modal" data-bs-target="#changeRating">Change Rating</button>
+                    <button id="${books[x].title + "\\" + books[x].author}" onclick="deleteBook(this.id)" class="btn delete-btn">Delete</button>
+                </div>
             </div>
-        </div>
-      `
-
-        document.getElementById(`bookRow${Math.floor(x/4)}`).innerHTML += newCard   
+        `
+            // add html to book row
+            document.getElementById(`bookRow${Math.floor(x/4)}`).innerHTML += newCard   
+        }
+    } catch (error) {
+        console.error("loadBooks() error: " + error)
     }
 }
 
+
+// delete book
 function deleteBook(titleAuthor){
+    // split the title from the author
     let title = titleAuthor.split("\\")[0]
     let author = titleAuthor.split("\\")[1]
     
+    // send request to delete book
     var request = new XMLHttpRequest()
-    request.open('DELETE', `http://localhost:5000/delReq/${title}/${author}`, false)
+    request.open('DELETE', `http://ec2-35-182-99-203.ca-central-1.compute.amazonaws.com:5000/delReq/${title}/${author}`, false)
     request.send()
+
+    // reload page
     location.reload()
 }
 
+
+// set change variables to book title and author so when changeRating() runs, it knows which book to change
 function setBookChange(titleAuthor){
     changeRatingTitle = titleAuthor.split("\\")[0]
     changeRatingAuthor = titleAuthor.split("\\")[1]
 }
 
-function changeRating(){
-    var request = new XMLHttpRequest()
 
+// change rating
+function changeRating(){
+    // get rating from form
     var newRating = document.getElementById("newRating").value
 
-    request.open('PUT', `http://localhost:5000/putReq/${changeRatingAuthor}/${changeRatingTitle}/${newRating}`, false)
+    // send change rating request
+    var request = new XMLHttpRequest()
+    request.open('PUT', `http://ec2-35-182-99-203.ca-central-1.compute.amazonaws.com:5000/putReq/${changeRatingAuthor}/${changeRatingTitle}/${newRating}`, false)
     request.send()
+
+    // reload page
     location.reload()
 }
-
-loadBooks()
